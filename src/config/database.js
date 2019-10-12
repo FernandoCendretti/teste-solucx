cb4b4d37ed2c097293fb283ef99ce700709898a2
FernandoCendretti <luiz.cendretti@gmail.com>
2019-10-11 21:29:55 -0300

    Commit Inicial

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 123123,
+  database: 'solucx_teste',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
