3bcaf510b939c5af4e2b00b94f1667a09d7b8d9f
FernandoCendretti <luiz.cendretti@gmail.com>
2019-10-14 20:06:07 -0300

    Correção na conexão com banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  username: 'root',
+  password: '123123',
+  database: 'solucx_teste',
   define: {
     timestamps: true,
     underscored: true,
